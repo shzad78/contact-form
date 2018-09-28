27400b3bbf337002f61b85a16ed23dc6d37157e3
shahzad babar <shzad78@gmail.com>
2018-09-28 09:53:15 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import ContactForm from './ContactForm'
+import {Route} from 'react-router-dom'
+import ContactForm from './ContactForm';
+import Home from './Home';
+import Faq from './Faq';
+import Contact from './Contact';
+import About from './About';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +14,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <Route exact path='/' component= {Home} />
+            <Route exact path='/about' component= {About} />
+            <Route path='/about/faq' component= {Faq} />
+            <Route exact path='/contact' component= {Contact} />
+          </div>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <ContactForm />
---
+++ src/Faq.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Faq extends Component {
+  render() {
+    return (
+      <div>
+        This is FAQ page
+      </div>
+    )
+  }
+}
